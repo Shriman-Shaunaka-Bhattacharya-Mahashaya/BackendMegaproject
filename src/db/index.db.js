fc64915fa6ce1ffb44c3b8b5ee3a26173680742f
Sounak Bhattacharya <bhattacharyasounak3@gmail.com>
2025-06-28 23:10:42 +0530

    db connection code added

---
+++ src/db/index.db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "../constants.js"
+
+const connectDB=async ()=>{
+    try{
+        console.log("MONGODB_URI:", process.env.MONGODB_URI);
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\nMONGODB connected !! DB_HOST: ${connectionInstance.connection.host}`);
+    }catch(error){
+        console.log("MONGODB connection error",error);
+        process.exit(1)
+    }
+}
+export default connectDB;
